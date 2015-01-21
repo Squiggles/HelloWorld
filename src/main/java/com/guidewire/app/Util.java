48d7b8a681311a50906556e161d8b5bfed339bea
Scott McDowell <mcdowesj@tcd.ie>
2015-01-21 10:08:24 +0000

    Added some methods and tests

--- src/main/java/com/guidewire/app/Util.java
+++ src/main/java/com/guidewire/app/Util.java
@@ -5,4 +5,16 @@ public class Util
     public static void printMessage(String msg) {
         System.out.println(msg);
     }
+
+    public static String toString(int[] numbers){
+        StringBuffer array = new StringBuffer();
+        if(numbers.length >0){
+            for(int a:numbers){
+                array.append(a);
+            }
+        }else {
+            return "Empty...";
+        }
+        return array.toString();
+    }
 }
--- src/test/java/com/guidewire/app/AppTest.java
+++ src/test/java/com/guidewire/app/AppTest.java
@@ -10,57 +10,71 @@ import java.util.Collections;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public AppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(AppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
+    public void testApp() {
 
-        assertTrue( true );
+        assertTrue(true);
     }
 
     /**
      * A beautiful unit test for the Sort.sort method
      */
-    public void testSort(){
+    public void testSort() {
         Sort sorter = new Sort();
-        int[] numbers = {4,6,2,7,4,6,4,5,3,2};
+        int[] numbers = {4, 6, 2, 7, 4, 6, 4, 5, 3, 2};
         int[] sortedNumbers = numbers;
         sorter.sort(numbers);
         ArrayList<Integer> numbersList = new ArrayList<Integer>();
         ArrayList<Integer> sortedNumbersList = new ArrayList<Integer>();
-        for(int i:numbers){
+        for (int i : numbers) {
             numbersList.add(i);
 
         }
-        for(int j : sortedNumbers) {
+        for (int j : sortedNumbers) {
             sortedNumbersList.add(j);
         }
         Collections.sort(sortedNumbersList);
         assertEquals(numbersList, sortedNumbersList);
+    }
 
 
+    public void testConcat() {
+        Sort mc = new Sort();
+        String result = mc.concat("one", "twwo");
+        assertEquals("onetwo", result);
+    }
+
+
+    public void testIsPrime() {
+        Sort mc = new Sort();
+        assertEquals(mc.isPrime(23), true);
+    }
+
+
+    public void testIsListPrime() {
+        Sort mc = new Sort();
+        int[] nums = {1, 3, 7};
+        assertEquals(mc.isListPrime(nums), true);
 
     }
 }
